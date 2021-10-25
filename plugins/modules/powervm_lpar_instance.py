f6cba8acaccc354794248eb6cada1a872071aece
Navinakumar <nkandak1@in.ibm.com>
2021-10-25 09:21:57 -0500

    indentation correction

--- plugins/modules/powervm_lpar_instance.py
+++ plugins/modules/powervm_lpar_instance.py
@@ -384,7 +384,7 @@ def validate_proc_mem(system_dom, proc, mem, proc_unit=None):
     else:
         if proc > curr_avail_procs:
             raise HmcError("{2} Available system proc units is not enough for {1} dedicated CPUs. Provide value on or below {0} CPUs"
-                            .format(str(int_avail_proc), str(proc), str(curr_avail_procs)))
+                           .format(str(int_avail_proc), str(proc), str(curr_avail_procs)))
 
     curr_avail_mem = system_dom.xpath('//CurrentAvailableSystemMemory')[0].text
     int_avail_mem = int(curr_avail_mem)
