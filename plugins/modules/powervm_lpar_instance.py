62e69e698d30bb658377fed513fd142f4d703e98
Navinakumar <nkandak1@in.ibm.com>
2022-08-11 00:41:06 -0500

    Fixed VNIC backing device check

--- plugins/modules/powervm_lpar_instance.py
+++ plugins/modules/powervm_lpar_instance.py
@@ -1171,7 +1171,7 @@ def create_partition(module, params):
                 module.fail_json(msg="There are no RMC Active VIOS available in the managed system")
             sriov_adapters_dom = server_dom.xpath("//SRIOVAdapters//SRIOVAdapter")
             sriov_dvc_col = rest_conn.create_sriov_collection(sriov_adapters_dom)
-            if sriov_dvc_col is None:
+            if not sriov_dvc_col:
                 module.fail_json(msg="There are no SRIOV Physical ports available in the managed system")
             rest_conn.add_vnic_payload(draft_template_dom, params['vnic_config'], sriov_dvc_col, vios_name_list)
             rest_conn.updatePartitionTemplate(draft_uuid, draft_template_dom)
